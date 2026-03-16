e59396b815c755d3b83d942a8e3dfb73a6d21a31
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-03-17 00:06:04 +0530

    implementing db logic

--- db_insertion.py
+++ db_insertion.py
@@ -3,6 +3,8 @@ import time
 import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
@@ -13,10 +15,23 @@ class DB_Connection():
             raise ValueError("DB credentials not found")
         self.client = AsyncIOMotorClient(db_credential)
         self.collection = self.client['Multi-Document-RAG']['searchable_docs']
+        
+    def get_embeddings():
+        docs_to_insert = []
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+        pass
+        
+    def embedding_and_inserting(self,file_path):
+        loader = PyPDFLoader(file_path.path)
+        data = loader.load()
+        pass
+    
     
     
     
 db_connection = DB_Connection()
+
+
 
 
 
